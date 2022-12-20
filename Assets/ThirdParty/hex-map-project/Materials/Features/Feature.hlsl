e48a17315c184f86c86c144b4ace079ab8838ce6
Stefano Cecere <stefano.cecere@krur.com>
2022-12-20 19:09:01 +0100

    HexMapProject

---
+++ Assets/ThirdParty/hex-map-project/Materials/Features/Feature.hlsl
@@ -0,0 +1,35 @@
+﻿#include "../HexCellData.hlsl"
+
+void GetVertexCellData_float (
+	UnityTexture2D GridCoordinatesTexture,
+	float3 WorldPosition,
+	bool EditMode,
+	out float2 Visibility
+) {
+	float2 gridUV = WorldPosition.xz;
+	gridUV.x *= 1 / (4 * 8.66025404);
+	gridUV.y *= 1 / (2 * 15.0);
+	float2 cellDataCoordinates = floor(gridUV.xy) + GridCoordinatesTexture.SampleLevel(
+		GridCoordinatesTexture.samplerstate, gridUV, 0
+	).rg;
+	cellDataCoordinates *= 2;
+
+	float4 cellData = GetCellData(cellDataCoordinates, EditMode);
+	Visibility.x = cellData.x;
+	Visibility.x = lerp(0.25, 1, Visibility.x);
+	Visibility.y = cellData.y;
+}
+
+void GetFragmentData_float (
+	UnityTexture2D BaseTexture,
+	float2 UV,
+	float3 WorldPosition,
+	float3 Color,
+	float2 Visibility,
+	out float3 BaseColor,
+	out float Exploration
+) {
+	float3 c = BaseTexture.Sample(BaseTexture.samplerstate, UV).rgb * Color;
+	BaseColor = c.rgb * Visibility.x;
+	Exploration = Visibility.y;
+}
---
+++ Assets/ThirdParty/hex-map-project/Materials/HexCellData.hlsl
@@ -0,0 +1,30 @@
+﻿TEXTURE2D(_HexCellData);
+SAMPLER(sampler_HexCellData);
+float4 _HexCellData_TexelSize;
+
+float4 FilterCellData (float4 data, bool editMode) {
+	if (editMode) {
+		data.xy = 1;
+	}
+	return data;
+}
+
+float4 GetCellData (float3 uv2, int index, bool editMode) {
+	float2 uv;
+	uv.x = (uv2[index] + 0.5) * _HexCellData_TexelSize.x;
+	float row = floor(uv.x);
+	uv.x -= row;
+	uv.y = (row + 0.5) * _HexCellData_TexelSize.y;
+	float4 data = SAMPLE_TEXTURE2D_LOD(_HexCellData, sampler_HexCellData, uv, 0);
+	data.w *= 255;
+	return FilterCellData(data, editMode);
+}
+
+float4 GetCellData (float2 cellDataCoordinates, bool editMode) {
+	float2 uv = cellDataCoordinates + 0.5;
+	uv.x *= _HexCellData_TexelSize.x;
+	uv.y *= _HexCellData_TexelSize.y;
+	return FilterCellData(
+		SAMPLE_TEXTURE2D_LOD(_HexCellData, sampler_HexCellData, uv, 0), editMode
+	);
+}
