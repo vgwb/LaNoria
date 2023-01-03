48398e3e4684a2e44a7c7336aceec87bb022d0de
Stefano Cecere <stefano.cecere@krur.com>
2023-01-03 07:35:08 +0100

    init

---
+++ Assets/_scripts/Hex/HexNeighbours.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace vgwb
+{
+    [ExecuteInEditMode]
+    public class HexNeighbours : MonoBehaviour
+    {
+
+        public Hex hex
+        {
+            get {
+                return Hex.FromWorld(transform.position);
+            }
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            foreach (Hex neighbour in hex.Neighbours()) {
+                Gizmos.DrawSphere(neighbour.ToWorld(), .25f);
+            }
+        }
+
+    }
+}
