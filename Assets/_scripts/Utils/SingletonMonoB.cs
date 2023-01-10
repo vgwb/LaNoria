655b79713069bf19421e57f8b9e4b43df2e3275c
Giacomo <giaco.salvadori@gmail.com>
2023-01-10 11:47:05 +0100

    add singleton classes

---
+++ Assets/_scripts/Utils/SingletonMonoB.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class SingletonMonoB<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T I
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (!instance)
+        {
+            if (typeof(T) != GetType())
+            {
+                Destroy(this);
+                throw new Exception("Singleton instance type mismatch!");
+            }
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+}
