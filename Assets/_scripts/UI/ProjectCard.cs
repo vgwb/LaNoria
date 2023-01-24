c50f4e2d10ab379179021d7c3d792bd1401362fe
Stefano Cecere <stefano.cecere@krur.com>
2023-01-24 11:11:50 +0100

    Ui About: projects colors

--- Assets/_scripts/UI/ProjectCard.cs
+++ Assets/_scripts/UI/ProjectCard.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace vgwb.lanoria
@@ -8,29 +9,46 @@ namespace vgwb.lanoria
     public class ProjectCard : MonoBehaviour
     {
         public TextMeshProUGUI Title;
-        public GameObject Cat_1;
-        public GameObject Cat_2;
-        public GameObject Cat_3;
-        public GameObject Cat_4;
+        public Material Mat_1;
+        public Material Mat_2;
+        public Material Mat_3;
+        public Material Mat_4;
+
+        public GameObject[] Cats;
 
         public void Init(ProjectData data)
         {
             Title.text = data.Title;
 
+            int CatCounter = 0;
+
             foreach (ProjectCategories cat in data.Sequence) {
-                switch (cat) {
-                    case ProjectCategories.Environment:
-                        break;
-                    case ProjectCategories.Equality:
-                        break;
-                    case ProjectCategories.Tech:
-                        break;
-                    case ProjectCategories.People:
-                        break;
-                }
-
+                Cats[CatCounter].SetActive(true);
+                Cats[CatCounter].GetComponent<Image>().material = GetCatMaterial(cat);
+                CatCounter++;
             }
+            if (CatCounter < 3) {
+                Cats[2].SetActive(false);
+            }
+            if (CatCounter < 4) {
+                Cats[3].SetActive(false);
+            }
+        }
 
+        private Material GetCatMaterial(ProjectCategories cat)
+        {
+            switch (cat) {
+                case ProjectCategories.Environment:
+                    return Mat_1;
+                case ProjectCategories.Equality:
+                    return Mat_2;
+                case ProjectCategories.Tech:
+                    return Mat_3;
+                case ProjectCategories.People:
+                    return Mat_4;
+                default:
+                    return null;
+            }
         }
 
     }
