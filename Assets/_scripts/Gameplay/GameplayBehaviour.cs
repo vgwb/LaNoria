57b5d8e50e6b949ab2769353c63e538a46f1f4da
Giacomo <giaco.salvadori@gmail.com>
2023-01-26 12:30:28 +0100

    reorganize gameplay classes

--- Assets/_scripts/Gameplay/GameplayBehaviour.cs
+++ Assets/_scripts/Gameplay/GameplayBehaviour.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using Lean.Touch;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,28 +20,273 @@ namespace vgwb.lanoria
     public class GameplayBehaviour : MonoBehaviour
     {
         #region Var
+        public GameObject CardPrefab;
         public delegate void GameplayStateEvent(GameplayState state);
         public GameplayStateEvent OnStateUpdate;
 
         [SerializeField] private GameplayState state;
+        private int chosenCardIndex;
+        private Placeable instancedPlaceable;
+        private ProjectData chosenProjectData;
+        private List<CardInGame> spawnedCards;
+        private UI_GameHUD UIGame;
+        private LeanSpawnWithFinger spawner;
+        private CardDealer dealer;
+        private ScoreManager scorer;
         #endregion
 
         #region MonoB
         void Awake()
         {
+            if (CardPrefab == null) {
+                Debug.LogError("CardDealer - Awake(): no card prefab defined!");
+            }
+
             state = GameplayState.None;
+            ResetValues();
+        }
+
+        private void OnDestroy()
+        {
+            EventsUnsubscribe();
         }
         #endregion
 
         #region Functions
-        public void SetState(GameplayState newState)
+        public void StartGame(GameplayManager manager)
+        {
+            dealer = manager.Dealer;
+            scorer = manager.Scorer;
+            UIGame = UI_manager.I.PanelGameHUD;
+            spawner = UIGame.Spawner;
+            
+            EventsSubscribe();
+            SetState(GameplayState.Intro);
+        }
+
+        public void EndGame()
+        {
+            EventsUnsubscribe();
+            SetState(GameplayState.End);
+        }
+
+        public void PauseGame()
+        {
+            SetState(GameplayState.Pause);
+        }
+
+        public void OnClickCard(Transform placeablePrefab, ProjectData projectData, int cardIndex)
+        {
+            if (placeablePrefab != null && spawner != null) {
+                if (instancedPlaceable != null) {
+                    Destroy(instancedPlaceable.gameObject);
+                }
+
+                chosenCardIndex = cardIndex;
+                chosenProjectData = projectData;
+                spawner.Prefab = placeablePrefab;
+                var texture = UICameraManager.I.GetUICameraTexture(chosenCardIndex);
+                UIGame.CardSelectionHUD(projectData.Title, texture);
+            }
+        }
+
+        public void ConfirmProject()
+        {
+            instancedPlaceable.OnProjectConfirmed();
+            instancedPlaceable.transform.parent = BoardManager.I.ProjectsContainer.transform;
+            scorer.UpdateScore(instancedPlaceable);
+            ResetProjectPanel();
+            ResetValues();
+
+            SetState(GameplayState.Drawing);
+        }
+
+        public void ResetProjectPanel()
+        {
+            spawner.Prefab = null;
+            UIGame.ResetProjectPanel();
+        }
+
+        public void OnProjectDrag()
+        {
+            if (instancedPlaceable != null) {
+                UIGame.SlideOnTheRight();
+            }
+        }
+
+        private void SetState(GameplayState newState)
         {
             if (state != newState) {
                 state = newState;
+                ReadState();
 
                 if (OnStateUpdate != null) {
                     OnStateUpdate(state);
                 }
+            }
+        }
+
+        private void ResetValues()
+        {
+            chosenCardIndex = -1;
+            instancedPlaceable = null;
+            chosenProjectData = null;
+            spawnedCards = new List<CardInGame>();
+        }
+
+        private void OnPrefabSpawned(GameObject clone)
+        {
+            instancedPlaceable = clone.GetComponent<Placeable>();
+            instancedPlaceable.SetupCellsColor(chosenProjectData);
+            UIGame.EnableFingerCanvas(false);
+            SubscribeToPlaceableEvents();
+        }
+
+        private void OnPrefabSelect()
+        {
+            CameraManager.I.EnableRotationWithFingers(false);
+            UIGame.PrefabSelectionHUD();
+        }
+
+        private void SubscribeToPlaceableEvents()
+        {
+            if (instancedPlaceable != null) {
+                instancedPlaceable.OnValidPositionChange += HandleBtnConfirm;
+                instancedPlaceable.OnStopUsingMe += StopUsingPlaceable;
+                instancedPlaceable.OnSelectMe += OnPrefabSelect;
+            }
+        }
+
+        private void UnsuscribeToPlaceableEvents()
+        {
+            if (instancedPlaceable != null) {
+                instancedPlaceable.OnValidPositionChange -= HandleBtnConfirm;
+                instancedPlaceable.OnStopUsingMe -= StopUsingPlaceable;
+                instancedPlaceable.OnSelectMe -= OnPrefabSelect;
+            }
+        }
+
+        private void HandleBtnConfirm()
+        {
+            if (instancedPlaceable != null) {
+                UIGame.EnableBtnConfirm(instancedPlaceable.IsValidPosition);
+            }
+        }
+
+        private void StopUsingPlaceable()
+        {
+            UnsuscribeToPlaceableEvents();
+            UIGame.EnableFingerCanvas(true);
+        }
+
+        private void EnableSpawner(bool enable)
+        {
+            if (spawner != null) {
+                spawner.enabled = enable;
+            }
+        }
+
+        /// <summary>
+        /// Clean the actual hand of cards.
+        /// </summary>
+        private void CleanHand()
+        {
+            var cards = UIGame.CardsInUI();
+            for (int i = 0; i < cards.Count; i++) {
+                Destroy(cards[i]);
+            }
+        }
+
+        /// <summary>
+        /// Draw a new hand deleting the actual cards in hand.
+        /// </summary>
+        private void DrawNewHand()
+        {
+            CleanHand();
+            spawnedCards = new List<CardInGame>();
+            var projectsData = dealer.DrawProjects();
+            int cardIndex = 0;
+            foreach (var projectData in projectsData) {
+                var cardInstance = Instantiate(CardPrefab, UIGame.GetHook(cardIndex).transform); // spawn the card inside the container
+                var cardComp = cardInstance.GetComponent<CardInGame>();
+                if (cardComp != null) {
+                    spawnedCards.Add(cardComp);
+                    cardComp.HideInBottomScreen();
+                    var cardTexture = UICameraManager.I.GetUICameraTexture(cardIndex);
+                    cardComp.InitCard(projectData, cardTexture); // initialize the card component
+                    // get the associated model and bind it to the card clickable area
+                    var associatedPrefab = GameplayConfig.I.GetProjectModelFromData(projectData);
+                    int indexToPass = cardIndex;
+                    cardComp.SetCardEvents(() => OnClickCard(associatedPrefab.transform, projectData, indexToPass));
+                    // spawn the object in camera UI
+                    UICameraManager.I.SpawnPrefabInCamera(cardIndex, associatedPrefab, projectData);
+                }
+                cardIndex++;
+            }
+
+            SoundManager.I.PlaySfx(SfxEnum.shuffle);
+        }
+
+        private void EventsSubscribe()
+        {
+            spawner.OnSpawnedClone += OnPrefabSpawned;
+            UIGame.OnProjectDragged += OnProjectDrag;
+            UIGame.BtnConfirm.onClick.AddListener(() => ConfirmProject());
+        }
+
+        private void EventsUnsubscribe()
+        {
+            spawner.OnSpawnedClone -= OnPrefabSpawned;
+            UIGame.OnProjectDragged -= OnProjectDrag;
+            UIGame.BtnConfirm.onClick.RemoveListener(() => ConfirmProject());
+        }
+
+        private void CardEntrance()
+        {
+            UIGame.SetCanvasInteractable(false);
+            float duration = GameplayConfig.I.CardAppearsTime;
+            Sequence mySequence = DOTween.Sequence();
+            float index = 0;
+            foreach (var card in spawnedCards) {
+                float time = (duration / 2.0f) * index++;
+                mySequence.Insert(time, card.Rect.DOAnchorPosY(0.0f, duration).OnComplete(() => SoundManager.I.PlaySfx(SfxEnum.card)));
+            }
+
+            mySequence.PrependInterval(duration);
+            mySequence.AppendCallback(() => {
+                SetState(GameplayState.Play);
+                UIGame.SetCanvasInteractable(true);
+            });
+        }
+
+        private void ReadState()
+        {
+            switch (state) {
+                default:
+                case GameplayState.None:
+                    break;
+                case GameplayState.Intro:
+                    UI_manager.I.Show(UI_manager.States.Play);
+                    UIGame.SetCanvasAlpha(0.0f);
+                    CameraManager.I.EnableAutoRotate(false);
+                    CameraManager.I.ResetToOriginalRotY(() => SetState(GameplayState.Setup));
+                    break;
+                case GameplayState.Setup:
+                    UIGame.SetScoreUI(0);
+                    ResetProjectPanel();
+                    float duration = GameplayConfig.I.FadeInGameCanvas;
+                    UIGame.FadeCanvas(1.0f, duration, () => SetState(GameplayState.Drawing));
+                    break;
+                case GameplayState.Drawing:
+                    DrawNewHand();
+                    CardEntrance();
+                    break;
+                case GameplayState.Play:
+                    break;
+                case GameplayState.End:
+                    break;
+                case GameplayState.Pause:
+                    break;
             }
         }
         #endregion
--- Assets/_scripts/Gameplay/GameplayManager.cs
+++ Assets/_scripts/Gameplay/GameplayManager.cs
@@ -13,42 +13,24 @@ namespace vgwb.lanoria
         public bool StartGameOnPlay = false;
         [Header("Cards")]
         public CardDealer Dealer;
-        public GameObject CardPrefab;
         [Header("Score")]
         public ScoreManager Scorer;
         [Header("State")]
         public GameplayBehaviour StateHandler;
 
-        public delegate void GameplayEvent();
-        public GameplayEvent OnHandDrawed;
-        public GameplayEvent OnProjectChosen;
-        public delegate void GameplayEventOneParam(Placeable placeable);
-        public GameplayEventOneParam OnProjectConfirmed;
-
-        private int chosenCardIndex;
         private UI_GameHUD UIGame;
         private LeanSpawnWithFinger spawner;
-        private Placeable instancedPlaceable;
-        private ProjectData chosenProjectData;
-        private List<CardInGame> spawnedCards;
         #endregion
 
         #region MonoB
         protected override void Awake()
         {
             base.Awake();
-
-            if (CardPrefab == null) {
-                Debug.LogError("CardDealer - Awake(): no card prefab defined!");
-            }
-
-            ResetValues();
         }
 
         private void Start()
         {
             UIGame = UI_manager.I.PanelGameHUD;
-            spawner = UIGame.Spawner;
             EventsSubscribe();
 
             if (StartGameOnPlay) {
@@ -66,76 +48,17 @@ namespace vgwb.lanoria
         public void StartGame()
         {
             BoardManager.I.EmptyProjectsContainer();
-            SetState(GameplayState.Intro);
+            StateHandler.StartGame(this);
         }
 
-        public void OnClickCard(Transform placeablePrefab, ProjectData projectData, int cardIndex)
+        public void EndGame()
         {
-            if (placeablePrefab != null && spawner != null) {
-                if (instancedPlaceable != null) {
-                    Destroy(instancedPlaceable.gameObject);
-                }
-
-                chosenCardIndex = cardIndex;
-                chosenProjectData = projectData;
-                spawner.Prefab = placeablePrefab;
-                var texture = UICameraManager.I.GetUICameraTexture(chosenCardIndex);
-                UIGame.CardSelectionHUD(projectData.Title, texture);
-
-                if (OnProjectChosen != null) {
-                    OnProjectChosen();
-                }
-            }
+            StateHandler.EndGame();
         }
 
-        public void ConfirmProject()
+        public void PauseGame()
         {
-            instancedPlaceable.OnProjectConfirmed();
-            instancedPlaceable.transform.parent = BoardManager.I.ProjectsContainer.transform;
-            Scorer.UpdateScore(instancedPlaceable);
-            ResetProjectPanel();
-            ResetValues();
-
-            if (OnProjectConfirmed != null) {
-                OnProjectConfirmed(instancedPlaceable);
-            }
-
-            SetState(GameplayState.Drawing);
-        }
-
-        public void ResetProjectPanel()
-        {
-            spawner.Prefab = null;
-            UIGame.ResetProjectPanel();
-        }
-
-        public void OnProjectDrag()
-        {
-            if (instancedPlaceable != null) {
-                UIGame.SlideOnTheRight();
-            }
-        }
-
-        private void ResetValues()
-        {
-            chosenCardIndex = -1;
-            instancedPlaceable = null;
-            chosenProjectData = null;
-            spawnedCards = new List<CardInGame>();
-        }
-
-        private void OnPrefabSpawned(GameObject clone)
-        {
-            instancedPlaceable = clone.GetComponent<Placeable>();
-            instancedPlaceable.SetupCellsColor(chosenProjectData);
-            UIGame.EnableFingerCanvas(false);
-            SubscribeToPlaceableEvents();
-        }
-
-        private void OnPrefabSelect()
-        {
-            CameraManager.I.EnableRotationWithFingers(false);
-            UIGame.PrefabSelectionHUD();
+            StateHandler.PauseGame();
         }
 
         private void OnScoreUpdate(int score)
@@ -143,161 +66,14 @@ namespace vgwb.lanoria
             UIGame.SetScoreUI(score);
         }
 
-        private void SubscribeToPlaceableEvents()
-        {
-            if (instancedPlaceable != null) {
-                instancedPlaceable.OnValidPositionChange += HandleBtnConfirm;
-                instancedPlaceable.OnStopUsingMe += StopUsingPlaceable;
-                instancedPlaceable.OnSelectMe += OnPrefabSelect;
-            }
-        }
-
-        private void UnsuscribeToPlaceableEvents()
-        {
-            if (instancedPlaceable != null) {
-                instancedPlaceable.OnValidPositionChange -= HandleBtnConfirm;
-                instancedPlaceable.OnStopUsingMe -= StopUsingPlaceable;
-                instancedPlaceable.OnSelectMe -= OnPrefabSelect;
-            }
-        }
-
-        private void HandleBtnConfirm()
-        {
-            if (instancedPlaceable != null) {
-                UIGame.EnableBtnConfirm(instancedPlaceable.IsValidPosition);
-            }
-        }
-
-        private void StopUsingPlaceable()
-        {
-            UnsuscribeToPlaceableEvents();
-            UIGame.EnableFingerCanvas(true);
-        }
-
-        private void EnableSpawner(bool enable)
-        {
-            if (spawner != null) {
-                spawner.enabled = enable;
-            }
-        }
-
-        /// <summary>
-        /// Clean the actual hand of cards.
-        /// </summary>
-        private void CleanHand()
-        {
-            var cards = UIGame.CardsInUI();
-            for (int i = 0; i < cards.Count; i++) {
-                Destroy(cards[i]);
-            }
-        }
-
-        /// <summary>
-        /// Draw a new hand deleting the actual cards in hand.
-        /// </summary>
-        private void DrawNewHand()
-        {
-            CleanHand();
-            spawnedCards = new List<CardInGame>();
-            var projectsData = Dealer.DrawProjects();
-            int cardIndex = 0;
-            foreach (var projectData in projectsData) {
-                var cardInstance = Instantiate(CardPrefab, UIGame.GetHook(cardIndex).transform); // spawn the card inside the container
-                var cardComp = cardInstance.GetComponent<CardInGame>();
-                if (cardComp != null) {
-                    spawnedCards.Add(cardComp);
-                    cardComp.HideInBottomScreen();
-                    var cardTexture = UICameraManager.I.GetUICameraTexture(cardIndex);
-                    cardComp.InitCard(projectData, cardTexture); // initialize the card component
-                    // get the associated model and bind it to the card clickable area
-                    var associatedPrefab = GameplayConfig.I.GetProjectModelFromData(projectData);
-                    int indexToPass = cardIndex;
-                    cardComp.SetCardEvents(() => OnClickCard(associatedPrefab.transform, projectData, indexToPass));
-                    // spawn the object in camera UI
-                    UICameraManager.I.SpawnPrefabInCamera(cardIndex, associatedPrefab, projectData);
-                }
-                cardIndex++;
-            }
-
-            SoundManager.I.PlaySfx(SfxEnum.shuffle);
-
-            if (OnHandDrawed != null) {
-                OnHandDrawed();
-            }
-        }
-
         private void EventsSubscribe()
         {
-            spawner.OnSpawnedClone += OnPrefabSpawned;
             Scorer.OnScoreUpdate += OnScoreUpdate;
-            UIGame.OnProjectDragged += OnProjectDrag;
-            UIGame.BtnConfirm.onClick.AddListener(() => ConfirmProject());
-            StateHandler.OnStateUpdate += ReadState;
         }
 
         private void EventsUnsubscribe()
         {
-            spawner.OnSpawnedClone -= OnPrefabSpawned;
             Scorer.OnScoreUpdate -= OnScoreUpdate;
-            UIGame.OnProjectDragged -= OnProjectDrag;
-            UIGame.BtnConfirm.onClick.RemoveListener(() => ConfirmProject());
-            StateHandler.OnStateUpdate -= ReadState;
-        }
-
-        private void CardEntrance()
-        {
-            UIGame.SetCanvasInteractable(false);
-            float duration = GameplayConfig.I.CardAppearsTime;
-            Sequence mySequence = DOTween.Sequence();
-            float index = 0;
-            foreach (var card in spawnedCards) {
-                float time = (duration/2.0f) * index++;
-                mySequence.Insert(time, card.Rect.DOAnchorPosY(0.0f, duration).OnComplete(() => SoundManager.I.PlaySfx(SfxEnum.card)));
-            }
-            
-            mySequence.PrependInterval(duration);
-            mySequence.AppendCallback(() => {
-                SetState(GameplayState.Play);
-                UIGame.SetCanvasInteractable(true);
-            });
-        }
-
-        private void ReadState(GameplayState state)
-        {
-            switch (state) {
-                default:
-                case GameplayState.None:
-                    break;
-                case GameplayState.Intro:
-                    UI_manager.I.Show(UI_manager.States.Play);
-                    UIGame.SetCanvasAlpha(0.0f);
-                    CameraManager.I.EnableAutoRotate(false);
-                    CameraManager.I.ResetToOriginalRotY(() => SetState(GameplayState.Setup));
-                    break;
-                case GameplayState.Setup:
-                    UIGame.SetScoreUI(0);
-                    ResetProjectPanel();
-                    float duration = GameplayConfig.I.FadeInGameCanvas;
-                    UIGame.FadeCanvas(1.0f, duration, () => SetState(GameplayState.Drawing));
-                    break;
-                case GameplayState.Drawing:
-                    DrawNewHand();
-                    CardEntrance();
-                    break;
-                case GameplayState.Play:
-                    break;
-                case GameplayState.End:
-                    break;
-                case GameplayState.Pause:
-                    break;
-            }
-        }
-
-        private void SetState(GameplayState state)
-        {
-            if (StateHandler != null) {
-                StateHandler.SetState(state);
-            }
         }
         #endregion
     }
