052a216b8fefd42f27c2132c182f6a7a28d014a5
Stefano Cecere <stefano.cecere@krur.com>
2023-03-08 11:23:38 +0100

    alternative (simmetric) tiles

--- Assets/_scripts/Data/TilesData.cs
+++ Assets/_scripts/Data/TilesData.cs
@@ -14,13 +14,17 @@ namespace vgwb.lanoria
         P2A = 21,
         P3A = 31,
         P3B = 32,
+        P3B_alt = 34,
         P3C = 33,
         P4A = 41,
         P4B = 42,
+        P4B_alt = 47,
         P4C = 43,
         P4D = 44,
+        P4D_alt = 48,
         P4E = 45,
-        P4F = 46
+        P4F = 46,
+        P4F_alt = 49
     }
 
     [CreateAssetMenu(menuName = "VGWB/Tiles Data")]
