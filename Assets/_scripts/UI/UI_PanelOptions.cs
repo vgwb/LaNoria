02e76eb0bac23e796aa4f8b41dc0d21040ba1812
Stefano Cecere <stefano.cecere@krur.com>
2023-03-15 12:44:17 +0100

    UI Panel options

--- Assets/_scripts/UI/UI_PanelOptions.cs
+++ Assets/_scripts/UI/UI_PanelOptions.cs
@@ -8,11 +8,23 @@ namespace vgwb.lanoria
     public class UI_PanelOptions : MonoBehaviour
     {
         public Button BtnClose;
+        public Button BtnCredits;
+
+        public Toggle ToggleSfx;
+        public Toggle ToggleMusic;
+        public Toggle ToggleTutorial;
+        public Toggle ToggleAccessibility;
 
         void Start()
         {
             BtnClose.onClick.AddListener(() => AppManager.I.OnHome());
             BtnClose.onClick.AddListener(() => SoundManager.I.PlaySfx(AudioEnum.click));
+            BtnCredits.onClick.AddListener(() => OnCredits());
+
+            ToggleSfx.SetIsOnWithoutNotify(true);
+            ToggleMusic.SetIsOnWithoutNotify(true);
+            ToggleTutorial.SetIsOnWithoutNotify(false);
+            ToggleAccessibility.SetIsOnWithoutNotify(false);
         }
 
         public void OpenPanel()
@@ -23,6 +35,11 @@ namespace vgwb.lanoria
         public void ClosePanel()
         {
             gameObject.SetActive(false);
+        }
+
+        private void OnCredits()
+        {
+            Application.OpenURL("https://vgwb.org/projects/lanoria/");
         }
     }
 }
